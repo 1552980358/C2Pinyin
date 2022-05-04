a5d23ed5ab0470a4fabea5f7099ef78fd688d958
1552980358 <1552980358@qq.com>
2022-05-04 12:22:54 +0800

    DictionaryItem.kt: Update constructor

--- src/main/kotlin/projekt/cloud/piece/c2/pinyin/dictionary/DictionaryItem.kt
+++ src/main/kotlin/projekt/cloud/piece/c2/pinyin/dictionary/DictionaryItem.kt
@@ -5,7 +5,11 @@ package projekt.cloud.piece.c2.pinyin.dictionary
  * @param text [String]
  * @param pinyin [List]<[String]>
  **/
-data class DictionaryItem(
-    val text: String,
-    var pinyin: List<String>
-)
+data class DictionaryItem(val text: String, var pinyin: List<String>) {
+    /**
+     * Constructor
+     * @param text [String]
+     * @param pinyin vararg of [String]
+     **/
+    constructor(text: String, vararg pinyin: String): this(text, pinyin.toList())
+}
