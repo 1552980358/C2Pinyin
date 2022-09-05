1e951a24786ed9ed174078a966b4a30ba58737e2
1552980358 <1552980358@qq.com>
2022-09-05 09:42:20 +0800

    Bump to 2.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "projekt.cloud.piece"
-version = "2.3.1"
+version = "2.3.2"
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "projekt.cloud.piece"
             artifactId = "c2pinyin"
-            version = "2.3.1"
+            version = "2.3.2"
 
             from(components["java"])
         }
