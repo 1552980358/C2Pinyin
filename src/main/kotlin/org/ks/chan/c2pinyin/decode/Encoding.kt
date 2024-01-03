e55cfe544e6f2c364266ef27f9b8b9750f4f41da
1552980358 <1552980358@qq.com>
2024-01-03 16:50:02 +0800

    Encoding: Update comments

--- src/main/kotlin/org/ks/chan/c2pinyin/decode/Encoding.kt
+++ src/main/kotlin/org/ks/chan/c2pinyin/decode/Encoding.kt
@@ -10,25 +10,70 @@ private const val OFFSET_PADDING_START = 0
 private const val OFFSET_BASE = 19968
 private const val OFFSET_ENCODING_PART_DIFFERENCE = 7000
 
+/**
+ * [Encoding]
+ * @access Internal
+ **/
 internal abstract class Encoding {
 
     private companion object Unused
 
+    /**
+     * [Encoding.indexesBase64]
+     * @access Protected
+     * @return [String]
+     *
+     * Base64 encoded [ByteArray] of [Encoding.indexes]
+     **/
     protected abstract val indexesBase64: String
+
+    /**
+     * [Encoding.paddingsBase64]
+     * @access Protected
+     * @return [String]
+     *
+     * Base64 encoded [ByteArray] of [Encoding.paddings]
+     **/
     protected abstract val paddingsBase64: String
 
+    /**
+     * [Encoding.offsetPaddingStart]
+     * @return [Int]
+     *
+     * Start padding for [Encoding.offset] of implementation of [Encoding]
+     **/
     open val offsetPaddingStart: Int
         get() = OFFSET_PADDING_START
 
-    @OptIn(ExperimentalEncodingApi::class)
-    val indexes by lazy { Base64.decode(indexesBase64) }
+    /**
+     * [Encoding.indexes]
+     * @return [ByteArray]
+     **/
+    val indexes by lazy {
+        @OptIn(ExperimentalEncodingApi::class)
+        Base64.decode(indexesBase64)
+    }
 
-    @OptIn(ExperimentalEncodingApi::class)
-    val paddings by lazy { Base64.decode(paddingsBase64) }
+    /**
+     * [Encoding.paddings]
+     * @return [ByteArray]
+     **/
+    val paddings by lazy {
+        @OptIn(ExperimentalEncodingApi::class)
+        Base64.decode(paddingsBase64)
+    }
 
+    /**
+     * [Encoding.offset]
+     * @return [Int]
+     **/
     val offset: Int
         get() = OFFSET_BASE + offsetPaddingStart
 
+    /**
+     * [Encoding.offsetRange]
+     * @return [IntRange]
+     **/
     val offsetRange: IntRange
         get() = offset.run {
             this ..< this + OFFSET_ENCODING_PART_DIFFERENCE
@@ -36,6 +81,13 @@ internal abstract class Encoding {
 
 }
 
+/**
+ * [Int.encoding]
+ * @access Internal
+ * @return [Encoding]
+ *
+ * Return matching [Encoding] implementation for [Int]
+ **/
 internal val Int.encoding: Encoding
     get() = when (this) {
         in EncodingUpper.offsetRange -> EncodingUpper
