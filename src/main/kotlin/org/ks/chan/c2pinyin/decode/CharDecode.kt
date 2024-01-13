368579dd10b18e657f370e563904f47d0af3ead5
1552980358 <1552980358@qq.com>
2024-01-13 18:32:04 +0800

    CharDecode: Update comments

--- src/main/kotlin/org/ks/chan/c2pinyin/decode/CharDecode.kt
+++ src/main/kotlin/org/ks/chan/c2pinyin/decode/CharDecode.kt
@@ -3,10 +3,27 @@ package org.ks.chan.c2pinyin.decode
 import org.ks.chan.c2pinyin.LetterCase
 import org.ks.chan.c2pinyin.dictionary.Dictionary
 
+/**
+ * [Char.decodeToPinyinTableIndex]
+ * @access Internal
+ * @param dictionary [Dictionary]
+ * @return [Int]
+ *
+ * Convert [Char] into index of pinyin table
+ **/
 internal fun Char.decodeToPinyinTableIndex(dictionary: Dictionary): Int {
     return dictionary.findWord(this)?.tableIndex ?: tableIndex
 }
 
+/**
+ * [Char.decodeToPinyin]
+ * @access Internal
+ * @param letterCase [LetterCase]
+ * @param dictionary [Dictionary]
+ * @return [String]
+ *
+ * Convert [Char] into pinyin [String]
+ **/
 internal fun Char.decodeToPinyin(letterCase: LetterCase, dictionary: Dictionary): String {
     return when (val tableIndex = decodeToPinyinTableIndex(dictionary)) {
         INDEX_JUMP -> toString()
