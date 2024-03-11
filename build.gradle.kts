47fe3aace5ef27b10e7ced542dab2c5647231fd3
1552980358 <1552980358@qq.com>
2024-03-11 18:18:44 +0800

    Reset project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "me.ks.chan"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
