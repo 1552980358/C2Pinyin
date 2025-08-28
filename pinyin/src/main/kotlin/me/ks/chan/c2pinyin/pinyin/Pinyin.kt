7840ee5aeb93ade8d6a4d035340b145501276cae
1552980358 <1552980358@qq.com>
2025-08-28 14:38:43 +0800

    pinyin: Add reading resource file pinyin.json

--- pinyin/src/main/kotlin/me/ks/chan/c2pinyin/pinyin/Pinyin.kt
+++ pinyin/src/main/kotlin/me/ks/chan/c2pinyin/pinyin/Pinyin.kt
@@ -1,7 +1,12 @@
 package me.ks.chan.c2pinyin.pinyin
 
+import java.io.InputStream
+import kotlin.jvm.Throws
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
 
 @Serializable
 sealed class Pinyin {
@@ -24,3 +29,23 @@ sealed class Pinyin {
 
 }
 
+private val PinyinList: List<Pinyin> by lazy { PinyinJsonFileStream.asPinyinList }
+
+private const val PinyinJsonFile = "pinyin.json"
+private inline val PinyinJsonFileStream: InputStream
+    @Throws(IllegalStateException::class)
+    get() = PinyinJsonFile.runCatching(Pinyin::class.java::getResourceAsStream)
+        .mapCatching { inputStream -> inputStream!! }
+        .onFailure { exception ->
+            throw IllegalStateException("Error while loading resource $PinyinJsonFile", exception)
+        }
+        .getOrThrow()
+
+private inline val InputStream.asPinyinList: List<Pinyin>
+    @OptIn(ExperimentalSerializationApi::class)
+    @Throws(IllegalStateException::class)
+    get() = runCatching<_, List<Pinyin>> { use(Json::decodeFromStream) }
+        .onFailure { exception ->
+            throw IllegalStateException("Error while reading resource $PinyinJsonFile", exception)
+        }
+        .getOrThrow()
