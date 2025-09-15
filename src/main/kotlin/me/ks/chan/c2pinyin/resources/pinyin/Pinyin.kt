9becf2c1cdc400f90fb171d75fb270e238f46112
1552980358 <1552980358@qq.com>
2025-09-16 00:53:18 +0800

    C2Pinyin: Temporary remove unused methods

--- src/main/kotlin/me/ks/chan/c2pinyin/resources/pinyin/Pinyin.kt
+++ src/main/kotlin/me/ks/chan/c2pinyin/resources/pinyin/Pinyin.kt
@@ -22,59 +22,11 @@ data class Pinyin internal constructor(
         @Throws(IndexOutOfBoundsException::class, IllegalStateException::class)
         operator fun get(index: Int): Pinyin = PinyinList[index]
 
-        @Throws(NoSuchElementException::class)
-        operator fun get(pair: Pair<PinyinInitial, PinyinVowel>): Pinyin {
-            val target = Pinyin(pair.first, pair.second)
-            return PinyinList.first { pinyin -> pinyin == target }
-        }
-
     }
 
     override operator fun equals(other: Any?): Boolean {
-        return when (val other = other) {
-            is Pair<*, *> -> {
-                initial == other.first && vowel == other.second
-            }
-            is Pinyin -> {
-                initial == other.initial && vowel == other.vowel
-            }
-            else -> { false }
-        }
+        return other is Pinyin && other.vowel == vowel && other.initial == initial
     }
-
-    // @Serializable
-    // @ConsistentCopyVisibility
-    // @SerialName("single-vowel")
-    // data class SingleVowel internal constructor(
-    //     @SerialName("vowel")
-    //     val vowel: PinyinVowel
-    // ): Pinyin() {
-    //
-    //     override operator fun equals(other: Any?): Boolean {
-    //         return other is SingleVowel && other.vowel == vowel
-    //     }
-    //
-    //     override fun hashCode(): Int = vowel.hashCode()
-    //
-    // }
-
-    // @Serializable
-    // @ConsistentCopyVisibility
-    // @SerialName("initial-and-vowelled")
-    // data class InitialAndVowelled internal constructor(
-    //     @SerialName("initial")
-    //     val initial: PinyinInitial,
-    //     @SerialName("vowel")
-    //     val vowel: PinyinVowel
-    // ): Pinyin() {
-    //
-    //     override operator fun equals(other: Any?): Boolean {
-    //         return other is InitialAndVowelled && other.initial == initial && other.vowel == vowel
-    //     }
-    //
-    //     override fun hashCode(): Int = 31 * initial.hashCode() + vowel.hashCode()
-    //
-    // }
 
 }
 
