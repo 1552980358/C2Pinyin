6e24bc29e6c43d1d9a68ebdf9dde8bb35876aac9
1552980358 <1552980358@qq.com>
2025-10-18 05:53:05 +0800

    C2Pinyin: Update dictionary saving methods

--- src/main/kotlin/me/ks/chan/c2pinyin/dictionary/Dictionary.kt
+++ src/main/kotlin/me/ks/chan/c2pinyin/dictionary/Dictionary.kt
@@ -1,5 +1,14 @@
 package me.ks.chan.c2pinyin.dictionary
 
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.OutputStream
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToStream
+
 @ConsistentCopyVisibility
 data class Dictionary
 internal constructor(
@@ -31,6 +40,45 @@ internal constructor(
     infix fun add(otherRecordList: List<Record>) = apply { this += otherRecordList }
 
     infix fun add(dictionary: Dictionary) = apply { this += dictionary }
+
+    /**
+     * [Dictionary.jsonString]
+     * @return [kotlin.String] Output json string instance
+     **/
+    fun jsonString(): String {
+        return Json.encodeToString(recordList)
+    }
+
+    /**
+     * [Dictionary.toFile]
+     * @param target [java.io.File] Write to targeted [java.io.File] instance
+     * @throws java.io.IOException
+     * @throws java.io.FileNotFoundException
+     * @throws java.lang.SecurityException
+     **/
+    @Throws(FileNotFoundException::class, IOException::class, SecurityException::class)
+    fun toFile(target: File) {
+        target.writeText(jsonString())
+    }
+
+    /**
+     * [Dictionary.outputStream]
+     * @param target [java.io.OutputStream] Write to targeted [java.io.OutputStream] instance [target]
+     * @throws java.io.IOException
+     *
+     * You should call [java.lang.AutoCloseable.close] the [target] by your self!
+     * Strongly recommended to use kotlin extension method [java.lang.AutoCloseable.use] cooperated with method
+     * referencing to [Dictionary.outputStream]. See following example
+     * ```kotlin
+     * val dictionary = Dictionary { /*...*/ }
+     *     .also { File("dictionary.json").outputStream().use(it::outputStream) }
+     * ```
+     **/
+    @Throws(IOException::class)
+    fun outputStream(target: OutputStream) {
+        @OptIn(ExperimentalSerializationApi::class)
+        Json.encodeToStream(recordList, target)
+    }
 
 }
 
