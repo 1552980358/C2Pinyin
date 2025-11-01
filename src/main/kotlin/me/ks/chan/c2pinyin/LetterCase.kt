32471bec7aff309720a549c328a38e220ff69c67
1552980358 <1552980358@qq.com>
2025-11-01 21:52:41 +0800

    C2Pinyin: Add letter case source comment

--- src/main/kotlin/me/ks/chan/c2pinyin/LetterCase.kt
+++ src/main/kotlin/me/ks/chan/c2pinyin/LetterCase.kt
@@ -3,13 +3,23 @@ package me.ks.chan.c2pinyin
 import me.ks.chan.c2pinyin.resources.pinyin.PinyinInitial
 import me.ks.chan.c2pinyin.resources.pinyin.PinyinVowel
 
+/**
+ * A sealed class indicating letter case for [me.ks.chan.c2pinyin.resources.pinyin.Pinyin]
+ **/
 sealed class LetterCase {
 
+    /**
+     * Data class for defining [LetterCase] for [me.ks.chan.c2pinyin.resources.pinyin.Pinyin] ([PinyinInitial] and [PinyinVowel] pair)
+     * @constructor [LetterCase] (Default: [Uppercase]), [LetterCase] (Default: [Lowercase])
+     **/
     data class Pair(
         val initial: LetterCase = Uppercase, val vowel: LetterCase = Lowercase
     ) {
 
         companion object {
+            /**
+             * default instance of [LetterCase.Pair]
+             **/
             val Default by lazy(LetterCase::Pair)
         }
 
@@ -18,6 +28,9 @@ sealed class LetterCase {
     internal abstract operator fun invoke(initial: PinyinInitial): String
     internal abstract operator fun invoke(vowel: PinyinVowel): List<String>
 
+    /**
+     * Set all letters (chars) as uppercase (capital letter)
+     **/
     data object Uppercase: LetterCase() {
 
         override fun invoke(initial: PinyinInitial): String {
@@ -30,6 +43,9 @@ sealed class LetterCase {
 
     }
 
+    /**
+     * Set all letters (chars) as lowercase (small letter)
+     **/
     data object Lowercase: LetterCase() {
 
         override fun invoke(initial: PinyinInitial): String {
@@ -42,6 +58,9 @@ sealed class LetterCase {
 
     }
 
+    /**
+     * Set string capitalized, i.e., first letter as capital letter while remaining small letter
+     **/
     data object Capitalize: LetterCase() {
 
         override fun invoke(initial: PinyinInitial): String {
@@ -49,7 +68,9 @@ sealed class LetterCase {
         }
 
         override fun invoke(vowel: PinyinVowel): List<String> {
-            return vowel.strings.mapIndexed { i, str -> if (i > 0) str else str.uppercase() }
+            return vowel.strings.mapIndexed { i, str ->
+                if (i > 0) { str.lowercase() } else { str.lowercase() }
+            }
         }
 
     }
