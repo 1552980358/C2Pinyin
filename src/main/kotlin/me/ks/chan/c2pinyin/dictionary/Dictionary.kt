ce89eb0901627158b7263c3bc2933f38a63ef64b
1552980358 <1552980358@qq.com>
2025-11-03 05:26:44 +0800

    C2Pinyin: Add `Dictionary` class comments

--- src/main/kotlin/me/ks/chan/c2pinyin/dictionary/Dictionary.kt
+++ src/main/kotlin/me/ks/chan/c2pinyin/dictionary/Dictionary.kt
@@ -9,6 +9,10 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.encodeToStream
 
+/**
+ * @param recordList [List]<[Record]>
+ * @constructor [List]<[Record]>
+ **/
 @ConsistentCopyVisibility
 data class Dictionary
 internal constructor(
@@ -16,33 +20,64 @@ internal constructor(
 ) {
 
     companion object Static {
+        /**
+         * Create a [DictionaryBuilder] instance
+         * @return [DictionaryBuilder]
+         */
         @JvmStatic
         fun builder() = DictionaryBuilder()
     }
 
+    /**
+     * For Kotlin user only: Add [record] to current [Dictionary] instance
+     * @param record [Record]
+     **/
     @JvmSynthetic
     operator fun plusAssign(record: Record) {
         recordList = (recordList + record).distinctRecordText
     }
 
+    /**
+     * For Kotlin user only: Add [addRecordList] to current [Dictionary] instance
+     * @param addRecordList [List]<[Record]>
+     **/
     @JvmSynthetic
-    operator fun plusAssign(otherRecordList: List<Record>) {
-        recordList = (recordList + otherRecordList).distinctRecordText
+    operator fun plusAssign(addRecordList: List<Record>) {
+        recordList = (recordList + addRecordList).distinctRecordText
     }
 
+    /**
+     * For Kotlin user only: Add [recordList] owned by [dictionary] to current [Dictionary] instance
+     * @param recordList [Dictionary]
+     **/
     @JvmSynthetic
     operator fun plusAssign(dictionary: Dictionary) {
         recordList = (recordList + dictionary.recordList).distinctRecordText
     }
 
+    /**
+     * Add [record] to current [Dictionary] instance for Java user.
+     * Use `+=` operator ([plusAssign]) for Kotlin DSL user.
+     * @param record [Record]
+     **/
     infix fun add(record: Record) = apply { this += record }
 
-    infix fun add(otherRecordList: List<Record>) = apply { this += otherRecordList }
+    /**
+     * Add [recordList] to current [Dictionary] instance for Java user.
+     * Use `+=` operator ([plusAssign]) for Kotlin DSL user.
+     * @param recordList [Dictionary]
+     **/
+    infix fun add(recordList: List<Record>) = apply { this += recordList }
 
+    /**
+     * Add [recordList] owned by [Dictionary] instance to current [Dictionary] instance for Java user.
+     * Use `+=` operator ([plusAssign]) for Kotlin DSL user.
+     * @param dictionary [Dictionary]
+     **/
     infix fun add(dictionary: Dictionary) = apply { this += dictionary }
 
     /**
-     * [Dictionary.jsonString]
+     * Get serialized json string of current [Dictionary] instance
      * @return [kotlin.String] Output json string instance
      **/
     fun jsonString(): String {
@@ -50,7 +85,7 @@ internal constructor(
     }
 
     /**
-     * [Dictionary.toFile]
+     * Save current [Dictionary] instance to targeted [java.io.File] instance
      * @param target [java.io.File] Write to targeted [java.io.File] instance
      * @throws java.io.IOException
      * @throws java.io.FileNotFoundException
@@ -62,17 +97,16 @@ internal constructor(
     }
 
     /**
-     * [Dictionary.outputStream]
-     * @param target [java.io.OutputStream] Write to targeted [java.io.OutputStream] instance [target]
-     * @throws java.io.IOException
-     *
      * You should call [java.lang.AutoCloseable.close] the [target] by your self!
+     *
      * Strongly recommended to use kotlin extension method [java.lang.AutoCloseable.use] cooperated with method
      * referencing to [Dictionary.outputStream]. See following example
      * ```kotlin
      * val dictionary = Dictionary { /*...*/ }
      *     .also { File("dictionary.json").outputStream().use(it::outputStream) }
      * ```
+     * @param target [java.io.OutputStream] Write to targeted [java.io.OutputStream] instance [target]
+     * @throws java.io.IOException
      **/
     @Throws(IOException::class)
     fun outputStream(target: OutputStream) {
@@ -82,5 +116,9 @@ internal constructor(
 
 }
 
+/**
+ * @param this [List]<[Record]> Distinct [this] by [Record.text]
+ * @return [List]<[Record]>
+ **/
 private inline val List<Record>.distinctRecordText: List<Record>
     get() = distinctBy(Record::text)
